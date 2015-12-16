92715d37643c67eb4f5ae95fae661f4fff9c4228
Justin Le <justin@jle.im>
2015-12-16 04:05:16 -0800

    finished up foundatins of modify module

--- src/Data/Neural/Recurrent/Modify.hs
+++ src/Data/Neural/Recurrent/Modify.hs
@@ -1,9 +1,10 @@
-{-# LANGUAGE DataKinds #-}
-{-# LANGUAGE GADTs #-}
-{-# LANGUAGE ExplicitNamespaces #-}
+{-# LANGUAGE DataKinds           #-}
+{-# LANGUAGE ExplicitNamespaces  #-}
+{-# LANGUAGE FlexibleContexts    #-}
+{-# LANGUAGE GADTs               #-}
+{-# LANGUAGE RankNTypes          #-}
 {-# LANGUAGE ScopedTypeVariables #-}
-{-# LANGUAGE RankNTypes #-}
-{-# LANGUAGE TypeOperators #-}
+{-# LANGUAGE TypeOperators       #-}
 
 module Data.Neural.Recurrent.Modify where
 
@@ -20,7 +21,6 @@ import GHC.TypeLits           as L
 import GHC.TypeLits.Witnesses
 import Linear.V
 import Type.Family.Nat hiding (type (+))
-import qualified Data.Vector  as V
 
 deleteLayer :: KnownNet i hs o
             => (forall j k. (KnownNat j, KnownNat k) => Node j a -> Node k a)
@@ -45,7 +45,7 @@ addLayer :: (KnownNet i hs o, KnownNat l)
          -> (forall j k. (KnownNat j, KnownNat k) => (RLayer j l a, RLayer l k a))
          -> Fin (Len (i ': hs))
          -> Network i hs o a
-         -> (OpaqueNet i o a)
+         -> OpaqueNet i o a
 addLayer mO mI fn nt =
     case fn of
       FZ ->
@@ -82,12 +82,12 @@ deleteNode ix fn nt =
                       let l'D :: FLayer (h - 1) o a
                           l'D = over (tFLayerNodes . traverse . tNodeWeights) (deleteV fn) l'
                       in  OpaqueNet $ lD `NetIL` NetOL l'D
-                    NetIL (l' :: RLayer h j a) nt' ->
+                    NetIL (l' :: RLayer h j a) nt'' ->
                       let l'D :: RLayer (h - 1) j a
                           l'D = RLayer (over (traverse . tRNodeIWeights) (deleteV fn)
                                          $ rLayerNodes l' )
                                        (rLayerState l')
-                      in  OpaqueNet $ lD `NetIL` l'D `NetIL` nt'
+                      in  OpaqueNet $ lD `NetIL` l'D `NetIL` nt''
           _ -> error "impossible!"
       IS ix' ->
         case nt of
@@ -96,29 +96,33 @@ deleteNode ix fn nt =
               OpaqueNet ntD -> OpaqueNet $ l `NetIL` ntD
           _ -> error "impossible!"
 
-addNode :: forall i hs o a f. (Applicative f, KnownNet i hs o)
-        => (forall j l. (KnownNat j, KnownNat l) => f (RNode j l a, V l a, a, V o a))
-        -> Int
+addNode :: forall i hs o a. KnownNet i hs o
+        => (forall j l. (KnownNat j, KnownNat l, KnownNat (l + 1)) => (RNode j (l + 1) a, V l a, a))
+        -> (forall k. KnownNat k => V k a)
         -> Fin (Len hs)
         -> Network i hs o a
-        -> f (OpaqueNet i o a)
-addNode mO mI fn nt =
+        -> OpaqueNet i o a
+addNode mInp mOut fn nt =
     case nt of
       NetIL (l :: RLayer i j a) nt' ->
         case fn of
           FZ -> withNatOp (%+) (Proxy :: Proxy j) (Proxy :: Proxy 1) $
-            case nt' of
-              NetOL l' -> flip fmap mO $ \(lNew, wsNew, sNew, l'New) ->
-                            let lA :: RLayer i (j + 1) a
-                                -- lA = RLayer ((`snocV` lNew) . liftA2 undefined wsNew $ rLayerNodes l)
-                                lA = RLayer ((`snocV` lNew) . undefined $ rLayerNodes l)
-                                            (rLayerState l `snocV` sNew)
-                                l'A :: FLayer (j + 1) o a
-                                l'A = FLayer $ liftA2 (\w -> over tNodeWeights (`snocV` w)) l'New $ layerNodes l'
-                            in  OpaqueNet $ lA `NetIL` NetOL l'A
-              NetIL l' nt'' -> undefined
-          FS fn' ->
-            flip fmap (addNode mO mI fn' nt') $
-              \o -> case o of
+            let (lNew, wsNew, sNew) = mInp
+                lA :: RLayer i (j + 1) a
+                lA = RLayer ((`snocV` lNew)
+                              . liftA2 (\w -> over tRNodeSWeights (`snocV` w)) wsNew
+                              $ rLayerNodes l)
+                            (rLayerState l `snocV` sNew)
+            in  case nt' of
+                  NetOL l' ->
+                    let l'A :: FLayer (j + 1) o a
+                        l'A = FLayer $ liftA2 (\w -> over tNodeWeights (`snocV` w)) mOut $ layerNodes l'
+                    in  OpaqueNet $ lA `NetIL` NetOL l'A
+                  NetIL (l' :: RLayer j k a) nt'' ->
+                    let l'A :: RLayer (j + 1) k a
+                        l'A = RLayer (liftA2 (\w -> over tRNodeIWeights (`snocV` w)) mOut $ rLayerNodes l')
+                                     (rLayerState l')
+                    in  OpaqueNet $ lA `NetIL` l'A `NetIL` nt''
+          FS fn' -> case addNode mInp mOut fn' nt' of
                       OpaqueNet ntA -> OpaqueNet $ l `NetIL` ntA
       NetOL _ -> error "impossible!"
