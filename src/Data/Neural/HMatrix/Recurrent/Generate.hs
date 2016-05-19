1f80504cf6dc7b0adca98b823f820851ba9b55e0
Justin Le <justin@jle.im>
2016-05-18 21:41:07 -0700

    mwc generators

--- src/Data/Neural/HMatrix/Recurrent/Generate.hs
+++ src/Data/Neural/HMatrix/Recurrent/Generate.hs
@@ -1,14 +1,21 @@
-{-# LANGUAGE ScopedTypeVariables #-}
 {-# LANGUAGE GADTs               #-}
+{-# LANGUAGE PolyKinds           #-}
+{-# LANGUAGE ScopedTypeVariables #-}
 
-module Data.Neural.HMatrix.Recurrent.Generate where
+module Data.Neural.HMatrix.Recurrent.Generate
+  ( randomFLayer, randomRLayer, randomNet
+  , randomFLayerMWC, randomRLayerMWC, randomNetMWC
+  )
+  where
 
+import Control.Monad.Primitive
 import Control.Monad.Random
 import Data.Neural.HMatrix.Recurrent
 import Data.Neural.HMatrix.Utility
 import Data.Neural.Types             (KnownNet)
 import GHC.TypeLits
 import GHC.TypeLits.List
+import System.Random.MWC
 
 randomFLayer
     :: forall m i o. (MonadRandom m, KnownNat i, KnownNat o)
@@ -16,6 +23,15 @@ randomFLayer
     -> m (FLayer i o)
 randomFLayer r = FLayer <$> randomVec r
                         <*> randomMat r
+
+randomFLayerMWC
+    :: forall m i o. (PrimMonad m, KnownNat i, KnownNat o)
+    => (Double, Double)
+    -> Gen (PrimState m)
+    -> m (FLayer i o)
+randomFLayerMWC r g = FLayer <$> randomVecMWC r g
+                             <*> randomMatMWC r g
+
 
 randomRLayer
     :: forall m i o. (MonadRandom m, KnownNat i, KnownNat o)
@@ -25,6 +41,17 @@ randomRLayer r = RLayer <$> randomVec r
                         <*> randomMat r
                         <*> randomMat r
                         <*> randomVec r
+
+randomRLayerMWC
+    :: forall m i o. (PrimMonad m, KnownNat i, KnownNat o)
+    => (Double, Double)
+    -> Gen (PrimState m)
+    -> m (RLayer i o)
+randomRLayerMWC r g = RLayer <$> randomVecMWC r g
+                             <*> randomMatMWC r g
+                             <*> randomMatMWC r g
+                             <*> randomVecMWC r g
+
 
 randomNet
     :: forall m i hs o. (MonadRandom m, KnownNet i hs o)
@@ -38,4 +65,18 @@ randomNet r = go natsList
     go nl = case nl of
               ØNL       -> NetOL <$> randomFLayer r
               _ :<# nl' -> NetIL <$> randomRLayer r <*> go nl'
+
+randomNetMWC
+    :: forall m i hs o. (PrimMonad m, KnownNet i hs o)
+    => (Double, Double)
+    -> Gen (PrimState m)
+    -> m (Network i hs o)
+randomNetMWC r g = go natsList
+  where
+    go :: forall j js. KnownNat j
+       => NatList js
+       -> m (Network j js o)
+    go nl = case nl of
+              ØNL       -> NetOL <$> randomFLayerMWC r g
+              _ :<# nl' -> NetIL <$> randomRLayerMWC r g <*> go nl'
 
--- src/Data/Neural/HMatrix/Utility.hs
+++ src/Data/Neural/HMatrix/Utility.hs
@@ -1,15 +1,21 @@
 {-# LANGUAGE PolyKinds           #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
-module Data.Neural.HMatrix.Utility where
+module Data.Neural.HMatrix.Utility
+  ( randomVec, randomMat
+  , randomVecMWC, randomMatMWC
+  )
+  where
 
-import Control.Lens                    ((<&>))
-import Control.Monad
-import Control.Monad.Random            as R
-import Data.Proxy
-import GHC.TypeLits
-import Numeric.LinearAlgebra.Static
-import qualified Numeric.LinearAlgebra as H
+import           Control.Lens                 ((<&>))
+import           Control.Monad
+import           Control.Monad.Primitive
+import           Control.Monad.Random         as R
+import           Data.Proxy
+import           GHC.TypeLits
+import           Numeric.LinearAlgebra.Static
+import qualified Numeric.LinearAlgebra        as H
+import qualified System.Random.MWC            as MWC
 
 randomVec :: forall n m. (MonadRandom m, KnownNat n)
           => (Double, Double) -> m (R n)
@@ -18,6 +24,17 @@ randomVec = fmap unrow . randomMat
 randomMat :: forall f n m. (MonadRandom f, KnownNat n, KnownNat m)
           => (Double, Double) -> f (L n m)
 randomMat (mn, mx) = getRandom
+                 <&> \i -> uniformSample i (konst mn) (konst mx)
+  where
+    s = natVal (Proxy :: Proxy n) * natVal (Proxy :: Proxy m)
+
+randomVecMWC :: forall n m. (PrimMonad m, KnownNat n)
+             => (Double, Double) -> MWC.Gen (PrimState m) -> m (R n)
+randomVecMWC r g = unrow <$> randomMatMWC r g
+
+randomMatMWC :: forall f n m. (PrimMonad f, KnownNat n, KnownNat m)
+             => (Double, Double) -> MWC.Gen (PrimState f) -> f (L n m)
+randomMatMWC (mn, mx) g = MWC.uniform g
                  <&> \i -> uniformSample i (konst mn) (konst mx)
   where
     s = natVal (Proxy :: Proxy n) * natVal (Proxy :: Proxy m)
