d9f00238e9aa0c901b384234b8b02e5716ed80e6
Justin Le <justin@jle.im>
2016-07-20 23:43:29 -0700

    fixed dropout compensation on all layers?

--- src/Data/Neural/HMatrix/Recurrent/Dropout.hs
+++ src/Data/Neural/HMatrix/Recurrent/Dropout.hs
@@ -132,39 +132,32 @@ genNetMaskMWC doRate g = go natsList
              <$> uniform g
 {-# INLINE genNetMaskMWC #-}
 
--- TODO: is this seriously wrong or what
 compensateDO
     :: forall i hs o. KnownNet i hs o
     => Double
     -> Network i hs o
     -> Network i hs o
-compensateDO d n =
-    case n of
-      NetOL _ -> n
-      NetIL (RLayer b wI wS s) n' ->
-        NetIL (RLayer b wI (konst (1 - d) * wS) s)
-              (zipNet (*) (*) (konstNet (1 - d)) n')
--- compensateDO d = \case
---     NetOL w   -> NetOL w
---     NetIL l n -> NetIL l (go n)
---   where
---     go  :: forall h hs'. KnownNat h
---         => Network h hs' o
---         -> Network h hs' o
---     go = \case NetOL w   -> NetOL (compFLayer w)
---                NetIL w n -> NetIL (compRLayer w) (go n)
---     compFLayer
---         :: forall i' o'. (KnownNat i', KnownNat o')
---         => FLayer i' o'
---         -> FLayer i' o'
---     compFLayer = \case
---         FLayer b w       -> FLayer (konst d' * b) (konst d' * w)
---     compRLayer = \case
---         RLayer b wI wS s -> RLayer (konst d' * b) (konst d' * wI) (konst d' * wS) s
-
---     d' = 1 / (1 - d)
+compensateDO d = \case
+    NetOL w   -> NetOL w
+    NetIL l n -> NetIL l (go n)
+  where
+    go  :: forall h hs'. KnownNat h
+        => Network h hs' o
+        -> Network h hs' o
+    go = \case NetOL w   -> NetOL (compFLayer w)
+               NetIL w n -> NetIL (compRLayer w) (go n)
+    compFLayer
+        :: forall i' o'. (KnownNat i', KnownNat o')
+        => FLayer i' o'
+        -> FLayer i' o'
+    compFLayer (FLayer b w) =
+        FLayer b (konst d' * w)
+    compRLayer
+        :: forall i' o'. (KnownNat i', KnownNat o')
+        => RLayer i' o'
+        -> RLayer i' o'
+    compRLayer (RLayer b wI wS s) =
+        RLayer b (konst d' * wI) (konst d' * wS) s
+    d' = 1 / (1 - d)
 {-# INLINE compensateDO #-}
 
-      -- NetOL w   -> NetOL (compLayer w)
-      -- NetIL w n -> NetIL (compLayer w) (go n)
-    -- d' = 1 / (1 - d)
--- src/Data/Neural/Recurrent/Dropout.hs
+++ src/Data/Neural/Recurrent/Dropout.hs
@@ -1,4 +1,5 @@
-{-# LANGUAGE GADTs #-}
+{-# LANGUAGE GADTs               #-}
+{-# LANGUAGE LambdaCase          #-}
 {-# LANGUAGE ScopedTypeVariables #-}
 
 module Data.Neural.Recurrent.Dropout where
@@ -12,22 +13,31 @@ import Data.Neural.Types
 import Linear
 
 compensateDO
-    :: forall i hs o a. (Num a, KnownNet i hs o)
+    :: forall i hs o a. Fractional a
     => a
     -> Network i hs o a
     -> Network i hs o a
-compensateDO d n =
-    case n of
-      NetOL _ -> n
-      NetIL l n' ->
-        let l'  = compensateSWeights l
-            -- n'' = runIdentity . tNetRLayers (Identity . compensateStates . compensateWeights) $ n'
-            n'' = runIdentity . tNetRLayers (Identity . compensateWeights) $ n'
-        in  NetIL l' n''
+compensateDO d = \case
+    NetOL w   -> NetOL w
+    NetIL l n -> NetIL l (go n)
   where
-    compensateWeights :: forall j k. RLayer j k a -> RLayer j k a
-    compensateWeights = over (tRLayerNodes . traverse . tRNodeWeights) (bimap ((1 - d) *^) ((1 - d) *^))
-    compensateSWeights :: forall j k. RLayer j k a -> RLayer j k a
-    compensateSWeights = over (tRLayerNodes . traverse . tRNodeSWeights) ((1 - d) *^)
-    -- compensateStates :: forall j k. (KnownNat j, KnownNat k) => RLayer j k a -> RLayer j k a
-    -- compensateStates = over tRLayerState ((1 - d) *^)
+    go  :: forall h hs'. ()
+        => Network h hs' o a
+        -> Network h hs' o a
+    go = \case NetOL w   -> NetOL (compFLayer w)
+               NetIL w n -> NetIL (compRLayer w) (go n)
+    compFLayer
+        :: forall i' o'. ()
+        => FLayer i' o' a
+        -> FLayer i' o' a
+    compFLayer = over (tFLayerNodes . traverse . tNodeWeights) (d' *^)
+    compRLayer
+        :: forall i' o'. ()
+        => RLayer i' o' a
+        -> RLayer i' o' a
+    compRLayer = over (tRLayerNodes . traverse . tRNodeWeights) $ \(wI, wS) ->
+        let wI' = d' *^ wI
+            wS' = d' *^ wS
+        in  wI' `seq` wS' `seq` (wI', wS')
+    d' = 1 / (1 - d)
+{-# INLINE compensateDO #-}
